5a1d9f89dae6ac83d12c54b530d7b2171548c04d
AndrewW-coder <Its.Andrew.Wang.2007@gmail.com>
2020-07-05 21:19:19 -0700

    homework

--- Coding/firstprogram.py
+++ Coding/firstprogram.py
@@ -1,6 +1,13 @@
+x = str(input('Please insert number here: '))
 l = []
-for i in range(5):
-    y = input('Insert something here: ')
-    l.append(y)
-    print(l)
-
+for i in range(0, len(x), 1):
+    l.append(x[i])
+reversel = []
+for i in range(len(l)-1, -1, -1):
+    reversel.append(l[i]) 
+    
+print(reversel)
+if l == reversel:
+    print("I'm a palindrone")
+else:
+    print("I'm not a palindrone")
