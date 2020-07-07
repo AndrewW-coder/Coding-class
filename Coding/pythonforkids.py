f4a9bfe75ed31bd333b233a291cc281b9568b038
AndrewW-coder <Its.Andrew.Wang.2007@gmail.com>
2020-07-07 16:41:57 -0700

    python practice

--- Coding/pythonforkids.py
+++ Coding/pythonforkids.py
@@ -1,9 +1,22 @@
-import turtle
-t = turtle.Pen()
-for x in range(1, 19):
-    t.forward(100)
-    if x % 2 == 0:
-        t.left(175)
-    else:
-        t.left(225)
+x = str(input('Enter phrase here: '))
+reversel = []
+l = []
+for i in range(0, len(x), 1):
+    l.append(x[i])
+for i in range(len(l) -1, -1, -1):
+    reversel.append(l[i])
+s = ''
+for i in range(0, len(reversel)):
+    s = s + reversel[i]
+print(s)
+
+
+
+
+
+
+
+
+
+
 
