bbc53ef6a387a701dd1e347d616d856cfd947b15
AndrewW-coder <Its.Andrew.Wang.2007@gmail.com>
2020-07-07 08:30:21 -0700

    python for kids

--- Coding/pythonforkids.py
+++ Coding/pythonforkids.py
@@ -1,2 +1,12 @@
-print(len('Andrew is Awesome'))
+import random
+num = random.randint(1,100)
+while True:
+    i = int(input('Enter a number here: '))
+    if i == num:
+        print('You guessed right!')
+        break
+    elif i < num:
+        print('Try higher')
+    elif i > num:
+        print('Try lower')
 
