3e342f5fba472ecc184ffa23505bf8347783fbb6
AndrewW-coder <Its.Andrew.Wang.2007@gmail.com>
2020-07-07 19:19:29 -0700

    challenges

--- Coding/pythonforkids.py
+++ Coding/pythonforkids.py
@@ -1,14 +1,10 @@
-x = str(input('Enter phrase here: '))
-reversel = []
-l = []
-for i in range(0, len(x), 1):
-    l.append(x[i])
-for i in range(len(l) -1, -1, -1):
-    reversel.append(l[i])
-s = ''
-for i in range(0, len(reversel)):
-    s = s + reversel[i]
-print(s)
+l = [1, 3, 7, 5]
+m = l[0]
+for x in range(1, len(l)):
+    if m < l[x]:
+        m = l[x]
+    elif m > l[x]:
+        print(m)
 
 
 
