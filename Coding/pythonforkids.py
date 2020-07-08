fde87be65b50153148b727dd3c5b2bceac77c2aa
AndrewW-coder <Its.Andrew.Wang.2007@gmail.com>
2020-07-07 19:40:53 -0700

    pythonforkids

--- Coding/pythonforkids.py
+++ Coding/pythonforkids.py
@@ -1,10 +1,8 @@
-l = [1, 3, 7, 5]
-m = l[0]
-for x in range(1, len(l)):
-    if m < l[x]:
-        m = l[x]
-    elif m > l[x]:
-        print(m)
+x = int(input('Inert number here: '))
+p = 1
+for i in range(x, 0, -1):
+    p = p * i
+print(p)
 
 
 
