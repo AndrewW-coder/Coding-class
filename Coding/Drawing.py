1df1b6bcc85c80dd1d8a97833c9e0f5d4d79d011
AndrewW-coder <Its.Andrew.Wang.2007@gmail.com>
2020-07-10 10:55:59 -0700

    drawing

--- Coding/Drawing.py
+++ Coding/Drawing.py
@@ -1,10 +1,16 @@
 import turtle
 t = turtle.Pen()
 s = 0
+c = 0
 def mysquare(size):
     for x in range(1,5):
         t.forward(size)
         t.left(90)
+def mycircle(size):
+    t.circle(c)
 for i in range(1, 50):
     mysquare(s)
-    s = s + 1
+    mycircle(c)
+    s = s + 2
+    c = c + 10
+turtle.done()
