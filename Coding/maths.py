8ac3dc8e8c7516f862998acdc5a481daefae2bfd
AndrewW-coder <Its.Andrew.Wang.2007@gmail.com>
2020-07-11 09:08:17 -0700

    maths

--- Coding/maths.py
+++ Coding/maths.py
@@ -1,14 +1,15 @@
 x = int(input('Insert number here: '))
 flag = 1
 def prime_number(x):
+    flag = 1
     for i in range(2, x):
         if x % i == 0:
             flag = 0
-        elif x % i != 0:
-            flag = 1
-    if flag == 0:
-        print('This is not a prime number.')
-    elif flag == 1:
+            break
+        
+    if flag == 1:
         print('This is a prime number.')
+    elif flag == 0:
+        print('This is not a prime number.')
 prime_number(x)
     
