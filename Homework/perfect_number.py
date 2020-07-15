d1a6519a40c81893b95e26b8cd8adec1a56d2343
AndrewW-coder <Its.Andrew.Wang.2007@gmail.com>
2020-07-15 09:38:28 -0700

    perfect_number

--- Homework/perfect_number.py
+++ Homework/perfect_number.py
@@ -1,10 +1,8 @@
 for num in range(1, 10000):
-    l = []
-    for i in range(1, num):
+    sum = 0
+    for i in range(1, int(num/2+1)):
         if num % i == 0:
-            l.append(i)
-    s = 0
-    for j in range(0, len(l)):
-        s = s + l[j]
-    if s == num:
+            sum += i
+    if num == sum:
         print(num)
+    
