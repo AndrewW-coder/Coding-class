551ee9a90988d5fe2f34e33b02019d858c351bcc
AndrewW-coder <Its.Andrew.Wang.2007@gmail.com>
2020-07-16 15:21:31 -0700

    date

---
+++ Homework/date.py
@@ -0,0 +1,9 @@
+# def cal_days(month, day, year):
+#     jan =  mar = may = jul = aug = octo = dec = 31
+#     apr = jun = sep = nov = 30
+#     if year % 4 == 0:
+#         feb = 29
+#     else:
+#         feb = 28
+def cal_days(month, day, year):
+    if month     
