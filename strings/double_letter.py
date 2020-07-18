fd0e20962e2469317ea6c931114387197dbfaa8d
AndrewW-coder <Its.Andrew.Wang.2007@gmail.com>
2020-07-18 11:24:02 -0700

    dub_letter

--- strings/double_letter.py
+++ strings/double_letter.py
@@ -1,6 +1,18 @@
-word = ('Hello')
-word2 = ('Helo')
-if word == word2:
-    print('True')
-else:
-    print('false')
+x = input('Insert a string here: ')
+def dub_word(x):
+    l = []
+    for i in range(0, len(x)):
+        l.append(x[i])
+    s = 0
+    
+    if str(l[s]) == str(l[s + 1]):
+        f = 'This word has a double letter.'
+    else:
+        f = 'This word does not have a double letter'
+    print(f)
+    return f
+    
+dub_word(x)
+
+
+
