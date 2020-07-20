0b063620a4911b8300665d1f44d916856a69f204
AndrewW-coder <Its.Andrew.Wang.2007@gmail.com>
2020-07-20 14:23:36 -0700

    anagram

---
+++ strings/anagram.py
@@ -0,0 +1,15 @@
+def is_anagram(name_1, name_2):
+    l1 = []
+    l2 = []
+    
+    for i in range(0, len(name_1)):
+        l1.append(name_1[i])
+    for j in range(0, len(name_2)):
+        l2.append(name_2[j])
+    if len(name_1) != len(name_2):
+        anagram = False
+    if len(name_1) == len(name_2):
+        if 
+    
+
+
