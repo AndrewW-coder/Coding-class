dd1e89bd6a6c0371c4ae5dc17f0322d54678c9c6
AndrewW-coder <Its.Andrew.Wang.2007@gmail.com>
2020-07-21 12:11:11 -0700

    middle letter

--- strings/Middle_letter.py
+++ strings/Middle_letter.py
@@ -1,14 +1,14 @@
-l = []
 def mid(word):
+    l = []
     for i in range(0, len(word)):
         l.append(word[i])
     if len(word) % 2 == 0:
-        mid_letter = print('')
+        mid_letter = ''
     else:
         x = int((len(word) - 1)/2)
-        mid_letter = print(word[x])
+        mid_letter = word[x]
     return mid_letter
-mid('chicken')
+print(mid('chicken'))
 
 
 
