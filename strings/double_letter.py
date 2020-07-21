5e9bc255317ce6ed484d627e36af9c1e45c8157a
AndrewW-coder <Its.Andrew.Wang.2007@gmail.com>
2020-07-21 12:10:59 -0700

    double letters

--- strings/double_letter.py
+++ strings/double_letter.py
@@ -3,23 +3,15 @@ def dub_word(x):
     l = []
     for i in range(0, len(x)):
         l.append(x[i])
-    #s = 0
     for s in range(0, len(l)-1):
         if str(l[s]) == str(l[s + 1]):
             f = 'This word has a double letter.'
             break
         else:
-            f = ('This word does not have a double letter')
-        # else:
-        #     f = 'This word does not have a double letter.'
-    # if str(l[s]) == str(l[s + 1]):
-    #     f = 'This word has a double letter.'
-    # else:
-    #     f = 'This word does not have a double letter'
-    print(f)
+            f = 'This word does not have a double letter'    
     return f
     
-dub_word(x)
+print(dub_word(x))
 
 
 
