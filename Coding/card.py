1e67dbd2c574c227d93c137e0f421351852bb007
AndrewW-coder <Its.Andrew.Wang.2007@gmail.com>
2020-07-22 10:19:54 -0700

    cards

---
+++ Coding/card.py
@@ -0,0 +1,14 @@
+import random
+l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+s = ['♠️', '♥️', '♣️', '♦️']
+for i in range(0, 4):
+    suit = random.randint(0, len(s)-1)
+    n = random.randint(0, len(l)-1)
+    print(' ____')
+    if l[n] < 10:
+        print('|' + str(l[n]) + s[suit] + '  |')
+    else:
+        print('|' + str(l[n]) + s[suit] + ' |')
+    print('|' + '    |')
+    print(' `````')
+    
