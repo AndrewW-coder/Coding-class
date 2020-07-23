36a6b144c551805cee8d9d704ec136f8e043f91d
AndrewW-coder <Its.Andrew.Wang.2007@gmail.com>
2020-07-23 16:57:55 -0700

    rockpaperscissors

---
+++ difficult/rockpaperscissors.py
@@ -0,0 +1,27 @@
+import random
+
+moves = ['Rock', 'Paper', 'Scissors']
+
+x = input('What do you choose? Rock, Paper, or Scissors: ').lower()
+
+r = random.randint(0, len(moves)-1)
+
+a = moves[r].lower()
+print('You have chosen to play ' + x)
+print('The computer has chosen to play ' + a)
+
+if a == x:
+    print('Tie!')
+
+if a == 'rock' and x == 'paper':
+    print('You won!')
+if x == 'rock' and a == 'paper':
+    print('You lost.')
+if a == 'rock' and x == 'scissors':
+    print('You lost.')
+if x == 'rock' and a == 'scissors':
+    print('You won!')
+if a == 'paper' and x == 'scissors':
+    print('You won!')
+if x == 'paper' and a == 'scissors':
+    print('You lost.')
