fd05b9d389b890582f69d6903fd1f61227328a4c
AndrewW-coder <Its.Andrew.Wang.2007@gmail.com>
2020-07-26 08:24:16 -0700

    binary

---
+++ maths/binary.py
@@ -0,0 +1,15 @@
+x = input('Do you want to (a)convert binary to base 10 or (b)convert base 10 to binary: ').lower()
+if x == 'b':
+    num = int(input('What number do you want to convert to binary: '))
+    b = ''
+    while num > 0:
+        b = str(num % 2) + b
+        num = num/2
+    print(b)
+    
+
+    
+        
+
+
+
