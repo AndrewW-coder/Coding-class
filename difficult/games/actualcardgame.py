be26a9f3833ef376dee1a083b7f4e638acbbfc70
AndrewW-coder <Its.Andrew.Wang.2007@gmail.com>
2020-07-27 09:25:06 -0700

    cardgame

---
+++ difficult/games/actualcardgame.py
@@ -0,0 +1,23 @@
+class card(object):
+    def __init__(self, value):
+        self.value = value
+card3 = card('3')
+card4 = card('4')
+card5 = card('5')
+card6 = card('6')
+card7 = card('7')
+card8 = card('8')
+card9 = card('9')
+card10 = card('10')
+cardJ = card('J')
+cardQ = card('Q')
+cardK = card('K')
+cardA = card('A')
+card2 = card('2')
+cardbigJoker = ('bigJoker')
+cardsmallJoker = ('smallJoker')
+
+player1 = []
+player2 = []
+player3 = []
+
